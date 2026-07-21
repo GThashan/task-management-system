edfc0af4fac467f9b9b52510297658241e5365f9
GThashan <tharinduh20@gmail.com>
2026-07-21 12:22:58 +0530

    database connection added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./config/database";
 
 dotenv.config();
 
@@ -9,17 +10,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-app.get("/", (req,res)=>{
-    res.json({
-        message:"TaskFlow API Running"
-    });
+app.get("/", (req, res) => {
+  res.json({
+    message: "TaskFlow API Running",
+  });
 });
-
 
 const PORT = process.env.PORT || 5000;
 
+const connectDatabase = async () => {
+  try {
+    const connection = await pool.getConnection();
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
+    console.log("MySQL Database Connected Successfully");
+
+    connection.release();
+  } catch (error) {
+    console.error("Database Connection Failed:", error);
+  }
+};
+
+connectDatabase();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
