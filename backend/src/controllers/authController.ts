f799652df250fa4815e4bc91270ab0057d20cb81
GThashan <tharinduh20@gmail.com>
2026-07-21 13:53:16 +0530

    authentication part created

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from "express";
+import pool from "../config/database";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    if(!email || !password) { return res.status(401).json({
+      message: "All feild are required",
+    })}
+
+    const [rows]: any = await pool.execute(
+      `SELECT * FROM users WHERE email=?`,[email],
+    );
+
+    if (rows.length === 0) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const user = rows[0];
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET!,
+      {
+        expiresIn: "1d",
+      },
+    );
+
+    res.json({
+      message: "Login successful",
+
+      token,
+
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import pool from "./config/database";
-
+import authRoutes from "./routes/authRoutes";
 dotenv.config();
 
 const app = express();
@@ -15,6 +15,7 @@ app.get("/", (req, res) => {
     message: "TaskFlow API Running",
   });
 });
+app.use("/api/auth",authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
